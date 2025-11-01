468741e2bdf2d0956bc810fdbb36e05349ee0c96
Stefan Milinovic <stefan.milinovic25@gmail.com>
2025-11-01 14:46:34 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { LoadingScreen } from "./components/LoadingScreen.jsx";
+import "./index.css"
+
+function App() {
+    return<>
+        <LoadingScreen />
+    </>
+}
+
+export default App
